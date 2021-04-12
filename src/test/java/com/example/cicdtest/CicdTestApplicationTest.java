aa47cd725dcf29ac9acdf339da9100892893c426
João Santos Silva <joao.s.silva@wit-software.com>
2021-04-12 12:11:35 +0100

    first commit

---
+++ src/test/java/com/example/cicdtest/CicdTestApplicationTest.java
@@ -0,0 +1,18 @@
+package com.example.cicdtest;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+
+public class CicdTestApplicationTest {
+
+    @Test
+    public void deveRetornarCodSucesso(){
+        RestAssured.given()
+                .when()
+                    .get("http://localhost:8080/")
+                .then()
+                    .statusCode(200);
+
+    }
+}
